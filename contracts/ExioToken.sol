1d1ed560744e7fc3c06b372ace77ca093ac8d9b8
nalbandyanmisha <nalbandyanmisha@gmail.com>
2019-01-08 15:39:58 +0400

    Add transfer tokens method

--- contracts/ExioToken.sol
+++ contracts/ExioToken.sol
@@ -1,12 +1,33 @@
 pragma solidity ^0.5.0;
 
 contract ExioToken {
-  // Constructor
-  // Set the total number of tokens
-  //Read the total number of tokens
+  string public name = 'Exio Token';
+  string public symbol = 'Exio';
+  string public standard = 'Exio Token v1.0';
   uint256 public totalSupply;
 
-  constructor() public {
-    totalSupply = 777777;
+  event Transfer(
+    address indexed _from,
+    address indexed _to,
+    uint256 _value
+  );
+
+  mapping(address => uint256) public balanceOf;
+
+  constructor(uint256 _initialSupply) public {
+    balanceOf[msg.sender] = _initialSupply;
+    totalSupply = _initialSupply;
+    //allocate the initial supply
+  }
+
+  function transfer(address _to, uint256 _value) public returns(bool success) {
+    require(balanceOf[msg.sender] >= _value);
+
+    balanceOf[msg.sender] -= _value;
+    balanceOf[_to] += _value;
+
+    emit Transfer(msg.sender, _to, _value);
+
+    return true;
   }
 }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
 var ExioToken = artifacts.require("./ExioToken.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(ExioToken);
+  deployer.deploy(ExioToken, 777777);
 };
